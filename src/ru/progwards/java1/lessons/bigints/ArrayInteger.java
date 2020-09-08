8236d2f0617bf090c0777a59b215d228a48c7a07
Vova777 <vova.klok.00@mail.ru>
2020-09-09 01:52:12 +0300

    H9 задача 1 и 2 и 3 для автопроверки без 3.4

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -1,4 +1,45 @@
 package ru.progwards.java1.lessons.bigints;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 public class ArrayInteger {
+
+    private static ArrayInteger arrayInteger = new ArrayInteger(0);
+    byte[] digits;
+    ArrayInteger(int n){
+        byte[] digits = new byte[n];
+        this.digits = digits;
+    }
+
+    void fromInt(BigInteger value){
+        value.toString();
+        int lengthOfBigInt = value.toString().length();
+         arrayInteger = new ArrayInteger(lengthOfBigInt);
+
+        for (int i = 0; i < lengthOfBigInt; i++){
+            arrayInteger.digits[i] =Byte.parseByte(value.toString().substring(i,i+1));
+        }
+   }
+       BigInteger toInt(){
+          int lengthOfArray = arrayInteger.digits.length;
+          String strNumberBigInt = "";
+
+          for (int i = 0; i < lengthOfArray; i++){
+              strNumberBigInt = strNumberBigInt + arrayInteger.digits[i];
+          }
+          return new BigInteger(strNumberBigInt);
+       }
+
+
+
+    public static void main(String[] args) {
+
+ArrayInteger a = new ArrayInteger(0);
+        a.fromInt(new BigInteger("723"));
+
+        System.out.println(Arrays.toString(arrayInteger.digits));
+        System.out.println(arrayInteger.toInt().toString());
+
+    }
 }
