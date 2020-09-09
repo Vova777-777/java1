3c09cadcf5c6c623f0f0c4cd25af81c543c2ad3a
Vova777 <vova.klok.00@mail.ru>
2020-09-09 21:41:15 +0300

    H9 для автопроверки полностью. Попытка 1

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class ArrayInteger {
 
     private static ArrayInteger arrayInteger = new ArrayInteger(0);
-    byte[] digits;
+    byte[] digits = new  byte[0];
     ArrayInteger(int n){
         byte[] digits = new byte[n];
         this.digits = digits;
@@ -19,6 +19,7 @@ public class ArrayInteger {
 
         for (int i = 0; i < lengthOfBigInt; i++){
             arrayInteger.digits[i] =Byte.parseByte(value.toString().substring(i,i+1));
+            this.digits = arrayInteger.digits;
         }
    }
        BigInteger toInt(){
@@ -31,18 +32,44 @@ public class ArrayInteger {
           return new BigInteger(strNumberBigInt);
        }
 
-    boolean add(ArrayInteger num){
-        boolean b = true;
-        return b;
+    boolean add(ArrayInteger num) {
+        boolean forReturn = false;
+        byte[] array = this.digits;
+        byte[] numArray = num.digits;
+        int leastLength = 0;
+        if (array.length <= numArray.length) leastLength = array.length;
+        else leastLength = numArray.length;
+        if (numArray.length > array.length) return false;
+        byte vspom = 0;
+        for (int i = 0; i < leastLength; i++) {
+            array[array.length - 1 - i] = (byte) (array[array.length - 1 - i] + numArray[numArray.length - 1 - i]);
+            if (array[array.length - 1 - i] >= 10 && (array.length - 1 - i) != 0) {
+                vspom = (byte) (array[array.length - 1 - i] - 10);
+                array[array.length - 1 - i] = 0;
+                array[array.length - 2 - i] = (byte) (array[array.length - 2 - i] + vspom);
+            } else if (array[array.length - 1 - i] >= 10 && (array.length - 1 - i) == 0) {
+                array[array.length - 1 - i] = 0;
+                forReturn = false;
+            } else forReturn = true;
+        }
+        return forReturn;
     }
+
+
 
     public static void main(String[] args) {
 
 ArrayInteger a = new ArrayInteger(0);
+ArrayInteger b = new ArrayInteger(1);
         a.fromInt(new BigInteger("723"));
+        b.fromInt(new BigInteger("234"));
 
-        System.out.println(Arrays.toString(arrayInteger.digits));
-        System.out.println(arrayInteger.toInt().toString());
+        System.out.println(Arrays.toString(a.digits));
+        System.out.println(Arrays.toString(b.digits));
+        System.out.println(a.toInt().toString());
+        System.out.println(a.add(b));
+
+
 
     }
 }
