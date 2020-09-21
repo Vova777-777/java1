ff8e8b62de95521b7cd2e725e02d90d530b20832
Vova777 <vova.klok.00@mail.ru>
2020-09-21 23:00:16 +0300

    H11 задача 1 исправленная для робота

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -5,43 +5,44 @@ package ru.progwards.java1.lessons.io2;
 public class PhoneNumber {
 
     public static String format(String phone) throws Exception {
-       String onlyNumPhone = "";
-       for (int i = 0; i < phone.length(); i++){
-           onlyNumPhone = onlyNumPhone + getNum(phone.substring(i,i+1));
-       }
-        System.out.println(onlyNumPhone);
+       StringBuilder phoneNumber = new StringBuilder(phone);
+       getOnlyNumbers(phoneNumber);
+       if (phoneNumber.length() > 11 || phoneNumber.length() <10) {throw new Exception();}
+       if (phoneNumber.length() == 11) phoneNumber.insert(0,'+');
+       else phoneNumber.insert(0,"+7");
+       phoneNumber.insert(2,"(");
+       phoneNumber.insert(6,")");
+       phoneNumber.insert(10,"-");
 
-       if (onlyNumPhone.length() > 11 || onlyNumPhone.length() <10) {throw new Exception();}
-       String firstValueOfNumber = "+7";
-       if (onlyNumPhone.length() == 11) onlyNumPhone = firstValueOfNumber + onlyNumPhone.substring(1);
-       else onlyNumPhone = firstValueOfNumber + onlyNumPhone;
-        System.out.println(onlyNumPhone);
-        StringBuilder stringBuilder = new StringBuilder(onlyNumPhone.substring(0,2)+ "(" + onlyNumPhone.substring(2,5) + ")" + onlyNumPhone.substring(5,8) + "-" + onlyNumPhone.substring(8));
-        System.out.println(stringBuilder.toString());
-        return stringBuilder.toString();
+        System.out.println(phoneNumber);
+        return phoneNumber.toString();
     }
 
-    public static String getNum(String a){
-        switch (a){
-            case "0": return a;
-            case "1": return a;
-            case "2": return a;
-            case "3": return a;
-            case "4": return a;
-            case "5": return a;
-            case "6": return a;
-            case "7": return a;
-            case "8": return a;
-            case "9": return a;
-            default: a = "";
+    public static StringBuilder getOnlyNumbers(StringBuilder PhoneNumber){
+        for (int i = 0; i < PhoneNumber.length(); i++) {
+            switch (PhoneNumber.substring(i,i+1)) {
+                case "0": break;
+                case "1": break;
+                case "2": break;
+                case "3": break;
+                case "4": break;
+                case "5": break;
+                case "6": break;
+                case "7": break;
+                case "8": break;
+                case "9": break;
+                default: PhoneNumber.deleteCharAt(i);
+            }
         }
-
-        return a;
+        return PhoneNumber;
     }
 
-    public static void main(String[] args) throws Exception {
-
-        format("7(108)3630950");
+    public static void main(String[] args)  {
+       try {
+           format("7(108)3630");
+       }catch (Exception e){
+           System.out.println("введите правильное количество цифр вашего номера");
+       }
 
     }
 }
