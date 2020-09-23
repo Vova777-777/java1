f10a8f4d431f15b4a6c44ac2d3455e91c4034776
Vova777 <vova.klok.00@mail.ru>
2020-09-24 00:34:52 +0300

    H11 все 3 задачи. проверка 1ая. возможна ошибка при реализации собственного исключения

---
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -0,0 +1,69 @@
+package ru.progwards.java1.lessons.io2;
+
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Censor {
+
+    public static class CensorException extends IOException{
+        public String fileName = "";
+        public CensorException(String fileName){
+            super();
+            this.fileName = fileName;
+        }
+        @Override
+        public String getMessage() {
+            return (fileName == null ? "" : " \"" + fileName + "\"") + super.getMessage();
+        }
+    }
+
+
+
+    public static void censorFile(String inoutFileName, String[] obscene) throws CensorException {
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(inoutFileName, "rw")){
+          int b = randomAccessFile.read();
+          String str = "";
+          while (b != -1){
+              str = str + (char) b;
+              b = randomAccessFile.read();
+          }
+
+          for ( int i = 0; i < obscene.length; i++){
+               if (str.contains(obscene[i])) {changeWardToStars(inoutFileName,str,obscene[i]);}
+            }
+
+        } catch (IOException e) {
+            CensorException censorExeption = new CensorException(inoutFileName);
+            throw censorExeption;
+        }
+    }
+
+    public static void changeWardToStars(String inoutFileName, String dateFromFile, String wardForChange){
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(inoutFileName, "rw")){
+            String starsForChange = "";
+            for (int i = 0; i < wardForChange.length(); i++){
+                starsForChange = starsForChange + "*";
+            }
+            int a = 0;
+            while (a != -1){
+                a = dateFromFile.indexOf(wardForChange,a);
+                randomAccessFile.seek(a);
+                randomAccessFile.writeBytes(starsForChange);
+                a++;
+            }
+        }catch (IOException e){
+            e.getMessage();
+        }
+    }
+
+    public static void main(String[] args){
+        String str = "loves much his";
+        try {
+            censorFile("B:/1/ne", str.split(" ") );
+        } catch (CensorException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- src/ru/progwards/java1/lessons/io2/Translator.java
+++ src/ru/progwards/java1/lessons/io2/Translator.java
@@ -11,25 +11,18 @@ public class Translator {
         this.outLang = outLang;
     }
 
-
-
     public  String translate(String sentence){
       String[] arrayNoPunct = sentence.replaceAll("\\p{Punct}","").split(" ");
       String[] arrayWithPunctuation = sentence.split(" ");
-
-
-        StringBuilder stringBuilderResult = new StringBuilder();
+      StringBuilder stringBuilderResult = new StringBuilder();
         for (int i = 0; i < arrayNoPunct.length; i++){
             for (int j = 0; j < inLang.length; j++){
                if (arrayNoPunct[i].equalsIgnoreCase(inLang[j])){ stringBuilderResult.append(outLang[j]);}
-
                else { continue;}
-
-               if (!(arrayNoPunct[i].equalsIgnoreCase(arrayWithPunctuation[i]))){
+                if (!(arrayNoPunct[i].equalsIgnoreCase(arrayWithPunctuation[i]))){
                    stringBuilderResult.append(arrayWithPunctuation[i].substring(arrayWithPunctuation[i].length()-1));}
                stringBuilderResult.append(" ");
             }
-
         }
 
         String result = getNeedRegister(arrayNoPunct, inLang, stringBuilderResult.toString().trim());
@@ -65,10 +58,9 @@ public class Translator {
         ArrayList listResult = new ArrayList();
           Collections.addAll(listResult,result.split(" "));
         StringBuilder stringBuilder = new StringBuilder();
-        int j = -1;
+
         for(int i = 0; i < arrayNoPunct.length; i++){
-            j++;
-            if(str.equals (arrayNoPunct[i])) {listResult.add(j,arrayWithPunctuation[i]); j++; }
+            if(str.equals (arrayNoPunct[i])) {listResult.add(i,arrayWithPunctuation[i]);  }
         }
         for (int i = 0; i < listResult.size(); i++){
             stringBuilder.append(listResult.get(i));
@@ -81,7 +73,7 @@ public class Translator {
         String[] inLang = {"make", "love", "not", "war"};
         String[] outLang = {"твори", "любовь", "не", "войну"};
 
-        System.out.println( new Translator(inLang, outLang).translate("Not war! love make!"));
+        System.out.println( new Translator(inLang, outLang).translate("Not war - love - make!"));
 
 
         Set<Character> PUNCT_SET = new HashSet<>(Arrays.asList(
@@ -90,6 +82,4 @@ public class Translator {
                 '_', '`', '{', '|', '}', '~'));
 
     }
-
-
 }
