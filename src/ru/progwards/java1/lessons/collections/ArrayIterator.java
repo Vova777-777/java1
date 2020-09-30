71af24bf543d3ece7c3eeb78980cac941f1a330d
Vova777 <vova.klok.00@mail.ru>
2020-09-30 22:46:49 +0300

    H12 2 и 0.5*3 задача для проверки роботом

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -1,10 +1,12 @@
 package ru.progwards.java1.lessons.collections;
 
+
 import java.util.Iterator;
 
     public class ArrayIterator<T> implements Iterator<T> {
 
         private T[] array;
+        int count = -1;
 
         ArrayIterator(T[] array) {
             this.array = array;
@@ -12,17 +14,28 @@ import java.util.Iterator;
 
         @Override
         public boolean hasNext() {
-            if (this.equals(array[array.length-1]))// TODO Auto-generated method stub
+            if (this.count == array.length - 1)// TODO Auto-generated method stub
             return false;
             else return true;
-
         }
 
         @Override
         public T next() {
-            if (this.hasNext())// TODO Auto-generated method stub
-            return this.next();
-           else return null;
+            count++;
+           // TODO Auto-generated method stub
+            return this.array[count];
+        }
+
+        public static void main(String[] args) {
+            Integer[] array = {42,12,33,22,11,444,66778};
+
+
+            ArrayIterator iterator = new ArrayIterator(array);
+            while (iterator.hasNext()){
+               String a = iterator.next().toString();
+                System.out.println(a);
+            }
+
         }
     }
 
