98cb3267dc898262d0005cd5222e7e67cb37deb4
Vova777 <vova.klok.00@mail.ru>
2020-10-06 11:20:47 +0300

    H13 задача1 на проверку роботом

---
+++ src/ru/progwards/java1/lessons/sets/SetOperations.java
@@ -0,0 +1,61 @@
+package ru.progwards.java1.lessons.sets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+/*1.1 Метод public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) - объединение множеств*/
+       public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2){
+             set1.addAll(set2);
+             return set1;
+        }
+
+//1.2 Метод public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) - пересечение множеств;
+        public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+            set1.retainAll(set2);
+            return set1;
+        }
+
+//1.3 Метод public static Set<Integer> difference(Set<Integer> set1, Set<Integer> set2) - разница множеств;
+        public static Set<Integer> difference(Set<Integer> set1, Set<Integer> set2){
+           set1.removeAll(set2);
+           return set1;
+        }
+//1.4 Метод public static Set<Integer> symDifference(Set<Integer> set1, Set<Integer> set2) - симметрическая разница;
+        public static Set<Integer> symDifference(Set<Integer> set1, Set<Integer> set2){
+           Set interesectionSet = new HashSet(set1);
+           interesectionSet.retainAll(set2);
+           set1.addAll(set2);
+           set1.removeAll(interesectionSet);
+           return set1;
+        }
+
+    public static void main(String[] args) {
+        //1.1
+        Set set1 = new HashSet();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+        Set set2 = new HashSet();
+        set2.add(1);
+        set2.add(12);
+        set2.add(13);
+        set2.add(14);
+        set2.add(15);
+        System.out.println(union(set1,set2));
+
+        //1.2
+        System.out.println(intersection(set1, set2));
+
+        //1.3
+        set1.add(777);
+        System.out.println(difference(set1,set2));
+
+        //1.4
+        set1.add(12);
+        set1.add(13);
+        System.out.println(symDifference(set1,set2));
+    }
+}
