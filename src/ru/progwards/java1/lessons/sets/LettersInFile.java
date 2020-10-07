8faf1955f0c04f88c99b336e3bda11a01e243864
Vova777 <vova.klok.00@mail.ru>
2020-10-07 17:03:22 +0300

    H13 задача1,2 на проверку роботом

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -6,7 +6,5 @@ public class LettersInFile {
 /*3.1 Метод public static String process(String fileName) - вернуть все буквы, которые встретились в файле,
 сконкатенированные в виде строки. Буквы должны быть упорядочены по алфавиту, типа “ADEF...”.
 Все возникающие исключения, по работе с потоками, пробросить выше.*/
-    public static String process(String fileName) throws Exception{
-        RandomAccessFile randomAccessFile = new RandomAccessFile("r")
-    }
+
 }
