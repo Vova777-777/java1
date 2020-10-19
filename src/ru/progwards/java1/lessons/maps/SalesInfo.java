34889a435c6249b6ff075b6cbbc3264eb8dd63aa
Vova777 <vova.klok.00@mail.ru>
2020-10-19 15:59:24 +0300

    H15 задача 3 для робота не окончательн

--- src/ru/progwards/java1/lessons/maps/SalesInfo.java
+++ src/ru/progwards/java1/lessons/maps/SalesInfo.java
@@ -1,24 +1,4 @@
 package ru.progwards.java1.lessons.maps;
-/*3.1 Реализовать метод public int loadOrders(String fileName) - вернуть количество успешно загруженных строк.
-Если в строке более или менее 4-x полей, или количество и сумма не преобразуются в числа - эту строку не загружаем.
-3.2 Реализовать метод public Map<String, Double> getGoods() - вернуть список товаров, отсортированный по наименованию
-товара. В String - наименование товара, в Double - общая сумма продаж по товару
-3.3 Реализовать метод public Map<String, AbstractMap.SimpleEntry<Double, Integer>> getCustomers() - вернуть список
-покупателей, отсортированный по алфавиту. В String  - ФИ, в Double - сумма всех покупок покупателя, в Integer -
-количество покупок
-3.4 Протестировать на данных из примера ниже*/
-
-/*Пример
-Иванов Сергей, iPhone 10X, 2, 150000
-Петрова Анна, наушники JBL, 2, 7000
-Иванов Сергей, чехол для iPhone, 1, 1000
-Петрова Анна, пакет пластиковый, 1, 3
-Радж Кумар, батарейка ААА, 1, 150
-Михаил Цикло, iPhone 10X, 1, 75000
-Радж Кумар, пакет пластиковый, 1, 3
-Михаил Цикло, пакет пластиковый, 1, 3
-Иванов Сергей, стекло защитное, 1, 1000
-Василий Пупкин, спички, 10, 10*/
 
 
 import java.io.IOException;
@@ -42,13 +22,13 @@ public class SalesInfo {
              String str = scanner.nextLine();
              String[] arr = str.split(", ");
              if (arr.length != 4) continue;
-             if (checkStringIsNumber(arr[2]) == true) countOfInt++;
-             if (checkStringIsNumber(arr[3]) == true) countOfInt++;
+             if (checkStringIsNumber(arr[2])) countOfInt++;
+             if (checkStringIsNumber(arr[3])) countOfInt++;
              if (countOfInt != 2) continue;
              else count++;
                 listNamesBuyer.add(arr[0]);
                 listGoods.add(arr[1]);
-                listHowMany.add(Integer.valueOf(2));
+                listHowMany.add(Integer.valueOf(arr[2]));
                 listMoney.add(Double.valueOf(arr[3]));
             }
         }catch (IOException e){
@@ -59,7 +39,7 @@ public class SalesInfo {
 
     public Boolean checkStringIsNumber(String str){
         try {
-            int number = Integer.valueOf(str);
+            Integer.valueOf(str);
             return true;
         }catch (NumberFormatException e){
             return false;
@@ -76,12 +56,8 @@ public class SalesInfo {
         return map;
     }
 
-
-
     Map<String, AbstractMap.SimpleEntry<Double, Integer>> getCustomers(){
-
         Map<String, AbstractMap.SimpleEntry<Double, Integer>> map = new TreeMap<>();
-
         for (int i = 0; i < listHowMany.size(); i++){
             if (map.containsKey(listNamesBuyer.get(i))){
                 AbstractMap.SimpleEntry<Double, Integer> entry = map.get(listNamesBuyer.get(i));
