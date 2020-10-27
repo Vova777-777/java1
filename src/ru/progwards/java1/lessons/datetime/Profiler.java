bd6fa09c5f455af9429b5b6febbc7deb0bd6a0ad
Vova777 <vova.klok.00@mail.ru>
2020-10-28 01:53:49 +0300

    H16 задача 2 попытка 9

---
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -0,0 +1,46 @@
+package ru.progwards.java1.lessons.datetime;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profiler {
+
+    Instant insEnter;
+    Instant insExit;
+    long timeWorkSection;
+    public static List<StatisticInfo> list = new ArrayList<>();
+
+
+
+/*войти в профилировочную секцию, замерить время входа.*/
+    public static void enterSection(String name){
+        new Profiler().insEnter = Instant.now();
+    }
+
+/*выйти из профилировочной секции. Замерить время выхода, вычислить промежуток времени между входом и
+выходом в миллисекундах.*/
+    public static void exitSection(String name){
+        Profiler profiler = new Profiler();
+        profiler.insExit = Instant.now();
+        long timeWorkSection = Duration.between(profiler.insEnter, profiler.insExit).toMillis();
+        System.out.println(timeWorkSection);
+    }
+
+    public static List<StatisticInfo> getStatisticInfo(){ //это пока чтоб не путался
+        return list;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        enterSection("1");
+        exitSection("1");
+
+
+
+    }
+
+}
---
+++ src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
@@ -0,0 +1,42 @@
+package ru.progwards.java1.lessons.datetime;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatisticInfo {
+    public String sectionName;
+    public int fullTime =(int) new Profiler().timeWorkSection;      //полное время выполнения секции в миллисекундах.
+    public int selfTime = fullTime;       /* чистое время выполнения секции в миллисекундах. Для вложенных секций,
+                                 из времени выполнения внешней секции нужно вычесть времена выполнения
+                                    вложенных секций*/
+    public int count;            /*количество вызовов. В случае, если вызовов более одного, fullTime и
+                               selfTime содержат суммарное время выполнения всех вызовов.*/
+
+    Instant insEnter = new Profiler().insEnter;
+    Instant insExit = new Profiler().insExit;
+Profiler profiler = new Profiler();
+
+    public void method(StatisticInfo statisticInfo){
+        if (profiler.list.isEmpty()) profiler.list.add(statisticInfo);
+        else if (profiler.list.contains(statisticInfo)) {method1(statisticInfo); method2(statisticInfo);}
+    }
+
+    public void method1(StatisticInfo st){
+        for (int i = 0; i < profiler.list.size(); i++){
+            if (profiler.list.get(i).sectionName.equals(st.sectionName)) {profiler.list.get(i).count++;
+                profiler.list.get(i).fullTime = st.fullTime + profiler.list.get(i).fullTime;
+                }
+        }
+    }
+
+    public void method2(StatisticInfo stat){
+        int a = -1;
+        for (int i = 0; i < profiler.list.size(); i++){
+            if (stat.insEnter.isAfter(profiler.list.get(i).insEnter)
+                    &&stat.insEnter.isBefore(profiler.list.get(i).insExit)) profiler.list.get(i).selfTime = profiler.list.get(i).selfTime - stat.selfTime;
+
+        }
+    }
+}
+
