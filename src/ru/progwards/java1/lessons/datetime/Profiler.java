0e719277b1d8d88c817b3d50f069d4a06badac7f
Vova777 <vova.klok.00@mail.ru>
2020-10-28 23:12:39 +0300

    H16 задача 2 попытка14

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -12,7 +12,7 @@ public class Profiler {
     long timeWorkSection = 0;
     public static List<StatisticInfo> list = new ArrayList<>();
     static Profiler profiler = new Profiler();
-
+   static List<StatisticInfo> list1 = new ArrayList<>();
 
 
 /*войти в профилировочную секцию, замерить время входа.*/
@@ -24,20 +24,47 @@ public class Profiler {
 выходом в миллисекундах.*/
     public static void exitSection(String name){
         profiler.insExit = Instant.now();
+        StatisticInfo statisticInfo = new StatisticInfo();
+        statisticInfo.sectionName = name;
         long timeWorkSection = Duration.between(profiler.insEnter, profiler.insExit).toMillis();
+         method(statisticInfo);
         System.out.println(timeWorkSection);
     }
 
     public static List<StatisticInfo> getStatisticInfo(){ //это пока чтоб не путался
-        return new StatisticInfo().list1;
+        return list1;
     }
 
+    public static void method(StatisticInfo statisticInfo){
+        if (list1.isEmpty()) list1.add(statisticInfo);
+        else if (list1.contains(statisticInfo.sectionName)) {method1(statisticInfo); method2(statisticInfo);}
+    }
 
+    public static void method1(StatisticInfo st){
+        for (int i = 0; i < list1.size(); i++){
+            if (list1.get(i).sectionName.equals(st.sectionName)) {list1.get(i).count++;
+                list1.get(i).fullTime = st.fullTime + list1.get(i).fullTime;
+            }
+        }
+    }
 
+    public static void method2(StatisticInfo stat){
+        for (int i = 0; i < list1.size(); i++){
+            if (stat.timeOfEnter.isAfter(list1.get(i).timeOfEnter)
+                    &&stat.timeOfEnter.isBefore(list1.get(i).timeOfEnter)) list1.get(i).selfTime = list1.get(i).selfTime - stat.selfTime;
+
+        }
+    }
 
     public static void main(String[] args) {
+        List<Integer> listInt = new ArrayList<>();
         enterSection("1");
+        for (int i = 0; i < 1000000; i++){
+            listInt.add(i);
+        }
         exitSection("1");
+        getStatisticInfo();
+
 
 
 
--- src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
+++ src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
@@ -13,33 +13,15 @@ public class StatisticInfo {
     public int count;            /*количество вызовов. В случае, если вызовов более одного, fullTime и
                                selfTime содержат суммарное время выполнения всех вызовов.*/
 
+    public Instant timeOfEnter = new Profiler().insEnter;
+    public Instant timeOfExit;
 
 
-    Profiler profiler = new Profiler();
-    Instant insEnter = profiler.insEnter;
-    Instant insExit = profiler.insExit;
-List<StatisticInfo> list1 = new ArrayList<>();
 
-    public void method(StatisticInfo statisticInfo){
-        if (list1.isEmpty()) list1.add(statisticInfo);
-        else if (list1.contains(statisticInfo)) {method1(statisticInfo); method2(statisticInfo);}
-    }
 
-    public void method1(StatisticInfo st){
-        for (int i = 0; i < list1.size(); i++){
-            if (list1.get(i).sectionName.equals(st.sectionName)) {list1.get(i).count++;
-                list1.get(i).fullTime = st.fullTime + list1.get(i).fullTime;
-                }
-        }
-    }
 
-    public void method2(StatisticInfo stat){
-        for (int i = 0; i < list1.size(); i++){
-            if (stat.insEnter.isAfter(list1.get(i).insEnter)
-                    &&stat.insEnter.isBefore(list1.get(i).insExit)) list1.get(i).selfTime = list1.get(i).selfTime - stat.selfTime;
 
-        }
-    }
+
 
 
 }
