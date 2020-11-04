81d38c30aa58247b744709da9b3871fd97d8c9b4
Vova777 <vova.klok.00@mail.ru>
2020-11-05 01:04:29 +0300

    H16 задача 2 попытка 47 мой вариант точно правильный

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -5,131 +5,96 @@ import java.time.Instant;
 import java.util.*;
 
 public class Profiler {
+    static Instant insEnter ;
+    static Instant insExit ;
+    static List<StatisticInfo> listStatisticInfo = new ArrayList<>();
+    static List<StatisticInfo> resultList = new ArrayList<>();
+    static ArrayDeque<StatisticInfo> stack = new ArrayDeque<>();
 
-    private static LinkedList<StatisticInfo> statisticInfos = new LinkedList<>();
-
-    private static ArrayDeque<StatisticInfo> stack = new ArrayDeque<>();
-
-    //войти в профилировочную секцию, замерить время входа./
-    public static void enterSection(String name) {
-        StatisticInfo staticInfo = createStatisticInfo(name);
-        stack.push(staticInfo);
-        statisticInfos.add(staticInfo);
+    public static void enterSection(String name){
+        insEnter = Instant.now();
+        stack.push(createStaticInfo(name));
     }
-
-    private static StatisticInfo createStatisticInfo(String name) {
+    public static StatisticInfo createStaticInfo(String name){
         StatisticInfo statisticInfo = new StatisticInfo();
         statisticInfo.sectionName = name;
-        statisticInfo.timeOfEnter = Instant.now();
+        statisticInfo.timeOfEnter = insEnter;
         statisticInfo.count++;
         return statisticInfo;
     }
-
     /*выйти из профилировочной секции. Замерить время выхода, вычислить промежуток времени между входом и
     выходом в миллисекундах.*/
-    public static void exitSection(String name) {
-        StatisticInfo si = stack.pop();
-        int indexOfSt = statisticInfos.lastIndexOf(si);
-        Instant insExit = Instant.now();
-        si.timeOfExit = insExit;
-        long timeWorkSection = Duration.between(si.timeOfEnter, insExit).toMillis();
-        si.fullTime = (int) timeWorkSection;
-
-        if (indexOfSt == statisticInfos.size() - 1) {
-            si.selfTime = si.fullTime;
-        } else {
-            StatisticInfo childSi = statisticInfos.get(indexOfSt + 1);
-            List<StatisticInfo> statisticInfosFiltered = filterBySectionName(Profiler.statisticInfos.subList(indexOfSt, statisticInfos.size()), childSi.sectionName);
-            int fullTimeChild = (int) fullTimeBySection(statisticInfosFiltered);
-            si.selfTime = si.fullTime - fullTimeChild;
+    public static void exitSection(String name){
+        StatisticInfo st = stack.pop();
+        insExit = Instant.now();
+        st.timeOfExit = insExit;
+        long timeWorkSection = Duration.between(st.timeOfEnter, st.timeOfExit).toMillis();
+        st.fullTime = (int) timeWorkSection;
+        st.selfTime = st.fullTime;
+        listStatisticInfo.add(st);
+    }
+    public static List<StatisticInfo> getStatisticInfo(){
+        if (listStatisticInfo.size() == 1) resultList.add(listStatisticInfo.get(0));
+        getSelfTimeForEach();
+        method2();
+        resultList.sort(comparator);
+        return resultList;
+    }
+    public static void getSelfTimeForEach(){
+        for (int i = 0; i < listStatisticInfo.size(); i++){
+            for (int j = 0; j < listStatisticInfo.size(); j++)
+                if (listStatisticInfo.get(i).timeOfEnter.isBefore(listStatisticInfo.get(j).timeOfEnter)
+                        && listStatisticInfo.get(i).timeOfExit.isAfter(listStatisticInfo.get(j).timeOfEnter))
+                    listStatisticInfo.get(i).selfTime = listStatisticInfo.get(i).selfTime - listStatisticInfo.get(j).selfTime;
         }
     }
-
-    private static long fullTimeBySection(List<StatisticInfo> statisticInfosFiltered) {
-        long fullTime = 0;
-        for (StatisticInfo si: statisticInfosFiltered) {
-            fullTime = fullTime + si.fullTime;
-        }
-        return fullTime;
-    }
-
-
-    public static List<StatisticInfo> getStatisticInfo() {
-        List<StatisticInfo> commonStatistic = new ArrayList<>();
-        statisticInfos.sort(comparator);
-
-        Set<String> uniqueNames = getUniqueStatisticInfoNames(statisticInfos);
-        for (String uniqueName : uniqueNames) {
-            List<StatisticInfo> statisticInfosFilteredByName = filterBySectionName(Profiler.statisticInfos, uniqueName);
-            if(statisticInfosFilteredByName.size() == 1) {
-                commonStatistic.add(statisticInfosFilteredByName.get(0));
-            }else {
-                StatisticInfo reducedStatisticInfo = statisticInfosFilteredByName.get(0);
-                for (int i = 1; i < statisticInfosFilteredByName.size(); i++) {
-                    reducedStatisticInfo = reducedStatisticInfo.plus(statisticInfosFilteredByName.get(i));
+    public static void method2(){
+        for (int i = 0; i < listStatisticInfo.size(); i ++){
+            StatisticInfo statisticInfo = listStatisticInfo.get(i);
+            if(resultList.contains(statisticInfo)) continue;
+            for (int j = i + 1; j <= listStatisticInfo.size(); j++){
+                if (j == listStatisticInfo.size()) break; //чтобы не было исключения по размеру списка
+                if (statisticInfo.equals(listStatisticInfo.get(j))){
+                    statisticInfo.fullTime = statisticInfo.fullTime + listStatisticInfo.get(j).fullTime;
+                    statisticInfo.count = statisticInfo.count + listStatisticInfo.get(j).count;
+                    statisticInfo.selfTime = statisticInfo.selfTime + listStatisticInfo.get(j).selfTime;
                 }
-                commonStatistic.add(reducedStatisticInfo);
             }
+            resultList.add(statisticInfo);
         }
-        commonStatistic.sort(comparator);
-        return commonStatistic;
     }
-
-    public static List<StatisticInfo> filterBySectionName(List<StatisticInfo> statisticInfos, String sectionName) {
-        List<StatisticInfo> filteredByName = new ArrayList<>();
-        for (StatisticInfo statisticInfo : statisticInfos) {
-            if (statisticInfo.sectionName.equals(sectionName)) {
-                filteredByName.add(statisticInfo);
-            }
+    static Comparator<StatisticInfo> comparator = new Comparator<StatisticInfo>() {
+        @Override
+        public int compare(StatisticInfo o1, StatisticInfo o2) {
+            if (o1.equals(o2))
+                return 0;
+            if (o1.sectionName.compareTo(o2.sectionName) > 0) return 1;
+            else return -1;
         }
-        return filteredByName;
-    }
-
-    public static Set<String> getUniqueStatisticInfoNames(List<StatisticInfo> statisticInfos) {
-        List<String> names = new LinkedList<>();
-        for (StatisticInfo statisticInfo : statisticInfos) {
-            names.add(statisticInfo.sectionName);
-        }
-        return new HashSet<>(names);
-    }
-
-    private static Comparator<StatisticInfo> comparator = (o1, o2) -> {
-        if (o1.equals(o2))
-            return 0;
-        if (o1.sectionName.compareTo(o2.sectionName) > 0) return 1;
-        else return -1;
     };
-
     public static void main(String[] args) throws InterruptedException {
         List<Integer> listInt = new ArrayList<>();
         enterSection("1");
         Thread.sleep(100);
-
         enterSection("Process2");
         Thread.sleep(200);
-       enterSection("Process3");
-       Thread.sleep(100);
-      exitSection("Process3");
-      exitSection("Process2");
-
-       enterSection("Process2");
-      Thread.sleep(200);
-       enterSection("Process3");
-      Thread.sleep(100);
-       exitSection("Process3");
+        enterSection("Process3");
+        Thread.sleep(100);
+        exitSection("Process3");
         exitSection("Process2");
-
+        enterSection("Process2");
+        Thread.sleep(200);
+        enterSection("Process3");
+        Thread.sleep(100);
+        exitSection("Process3");
+        exitSection("Process2");
         exitSection("1");
-
-
         enterSection("1");
         Thread.sleep(100);
         exitSection("1");
-
         enterSection("1");
         Thread.sleep(100);
         exitSection("1");
         System.out.println(getStatisticInfo());
     }
-
 }
