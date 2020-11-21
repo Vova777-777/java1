c23619216d7da0e5e739525c12008dd26a0689e6
Vova777 <vova.klok.00@mail.ru>
2020-11-22 00:34:29 +0300

    H17 задача 1 попытка 4

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -12,72 +12,71 @@ public List<List<String>> findDuplicates(String startPath), результат -
 и полными путями совпадающих файлов.
  */
     public List<List<String>> findDuplicates(String startPath) throws IOException {
-        List  listAllFiles = new ArrayList<>();
+        List listAllFiles = new ArrayList<>();
         List<List<String>> result = new ArrayList<>();
         Path path = Paths.get(startPath);
-        Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
             @Override
-            public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-                    System.out.println(path);
+            public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                System.out.println(path);
                 listAllFiles.add(path);
                 return FileVisitResult.CONTINUE;
             }
+
             @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            public FileVisitResult visitFileFailed(Path file, IOException exc) {
                 return FileVisitResult.CONTINUE;
             }
         });
 
-        Set<Path> set = new HashSet();
-        for (int i = 0; i < listAllFiles.size() - 1; i++){
+        Set<Path> set = new HashSet<>();
+        for (int i = 0; i < listAllFiles.size() - 1; i++) {
             Path path1 = (Path) listAllFiles.get(i);
             for (int j = i + 1; j < listAllFiles.size(); j++) {
                 Path path2 = (Path) listAllFiles.get(j);
-                if (!checkNameOfFile(path1, path2)) continue;
-                else if (!checkLastModifiedTime(path1, path2)) continue;
-                else if (!checkSizeOfFile(path1, path2)) continue;
-                else if (!checkContentOfFile(path1, path2)) continue;
-                else {set.add(path1); set.add(path2);}
+                if (checkAllParameters(path1, path2)) {
+                    set.add(path1);
+                    set.add(path2);
+                }
             }
-                if (set.isEmpty()) continue;
-                result.add(new ArrayList(set));
-                set.clear();
+            result.add(new ArrayList(set));
+            set.clear();
         }
         return result;
     }
 
-    private boolean checkNameOfFile(Path path1, Path path2) throws Exception{
-        if (path1.getFileName().equals(path2.getFileName())) return true;
-        else return false;
+    private boolean checkAllParameters(Path path1, Path path2) throws IOException {
+        if (path1 == null || path2 == null) return false;
+        else
+            return checkContentOfFile(path1, path2) && checkLastModifiedTime(path1, path2)
+                    && checkNameOfFile(path1, path2) && checkSizeOfFile(path1, path2);
+    }
+
+    private boolean checkNameOfFile(Path path1, Path path2) {
+        return path1.getFileName().equals(path2.getFileName());
     }
 
     private boolean checkLastModifiedTime(Path path1, Path path2) throws IOException {
-        if (Files.getLastModifiedTime(path1).equals(Files.getLastModifiedTime(path2))) return true;
-        else return false;
+        return Files.getLastModifiedTime(path1).equals(Files.getLastModifiedTime(path2));
     }
 
     private boolean checkSizeOfFile(Path path1, Path path2) throws IOException {
-      if (Files.size(path1) == Files.size(path2)) return true;
-      else return false;
+        return Files.size(path1) == Files.size(path2);
     }
 
-    private boolean checkContentOfFile(Path path1, Path path2){
+    private boolean checkContentOfFile(Path path1, Path path2) {
         byte[] array1 = null;
         byte[] array2 = null;
         try {
             array1 = Files.readAllBytes(path1);
             array2 = Files.readAllBytes(path2);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if (Arrays.equals(array1, array2)) return true;
-        else return false;
-
+        return Arrays.equals(array1, array2);
     }
 
     public static void main(String[] args) throws IOException {
         System.out.println(new FindDuplicates().findDuplicates("B:/1"));
     }
-
 }
