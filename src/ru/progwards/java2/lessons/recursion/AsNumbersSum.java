cc78faaaac71ab6d6a2afb894b394f98fbb5847f
Vova777 <vova.klok.00@mail.ru>
2021-05-11 01:15:36 +0300

    must only to change sort like Integer

--- src/ru/progwards/java2/lessons/recursion/AsNumbersSum.java
+++ src/ru/progwards/java2/lessons/recursion/AsNumbersSum.java
@@ -4,43 +4,87 @@ package ru.progwards.java2.lessons.recursion;
 //public static String asNumbersSum(int number), который раскладывает параметр number, как всевозможные уникальные
 //        комбинации сумм натуральных чисел, например:
 //        5 = 4+1 = 3+2 = 3+1+1 = 2+2+1 = 2+1+1+1 = 1+1+1+1+1
-//        5 = 4+1 = 3+2 = 3+1+1 = 2+2+1 = 2+1+1+1 = 1+1+1+1+1
+//        7= 6+1= 5+2= 5+1+1= 4+3= 4+2+1= 4+1+1+1= 3+3+1= 3+2+2= 3+2+1+1= 3+1+1+1+1= 2+2+2+1= 2+2+1+1+1= 2+1+1+1+1+1= 1+1+1+1+1+1+1
 //        Строка должна содержать результат, отформатированный точно, как указано в примере.
 //        Повторные комбинации не допускаются, например, если а строке уже есть 3+2, то 2+3 там быть не должно.
 //        Задача должна быть решена методом рекурсии, циклы использовать запрещено.
 
-public class AsNumbersSum {
+import java.util.*;
 
-// 7 = 6+1 = 5+2 = 4+3 = 5+1+1 = 4+2+1 = 3+3+1 = 4+1+1+1 = 3+2+1+1 = 3+1+1+1+1 = 2+2+1+1+1 = 2+1+1+1+1+1 = 1+1+1+1+1+1+1
-// 7 = 6+1 = 5+2 = 4+3 = 5+1+1 = 4+2+1 = 3+3+1 = 4+1+1+1 = 3+2+1+1 = 3+1+1+1+1 = 2+2+1+1+1 = 2+1+1+1+1+1 = 1+1+1+1+1+1+1
+public class AsNumbersSum {
+// 7= 6+1= 5+2= 5+1+1= 4+3= 4+2+1= 4+1+1+1= 3+3+1= 3+2+2= 3+2+1+1= 3+1+1+1+1= 2+2+2+1= 2+2+1+1+1= 2+1+1+1+1+1= 1+1+1+1+1+1+1
+
+    static private Integer count = 0;
+    static TreeSet<String> setOfDifferentSum = new TreeSet<>();
 
     public static String asNumbersSum(int number){
-        String result = "";
-        String additionalStr = "";
-        result = getNumberAsTwoSummand(number,result, number, additionalStr) +
-                getNumberMoreThanTwoSummand(number, result, number, additionalStr);
-        return number + result;
+        getNumberAsTwoSummands("", number, "", number, "") ;
+        List<String> list = new ArrayList<>(setOfDifferentSum);
+        setOfDifferentSum = new TreeSet<>();
+        Collections.reverse(list);
+        return number + String.join("", list);
     }
 
-    private static String getNumberAsTwoSummand(int number, String result, int firstNumber, String additionalStr) {
-        if (isFinishCalculation(number, firstNumber)) return result;
-        result = result + " = " + (number - 1) + "+" + (firstNumber - number + 1) + additionalStr;
-        return getNumberAsTwoSummand(number-1, result, firstNumber, additionalStr);
+    private static String getNumberAsTwoSummands(String firstAddStr, int number, String result, int firstNumber, String additionalStr) {
+        count++;
+        int firstSummand = number - 1;
+        int secondSummand = (firstNumber - number + 1);
+        //result +=" = "+ firstAddStr +""+firstSummand + "+" + secondSummand + "+" + additionalStr;
+        String sum = firstAddStr +  firstSummand + "+" + secondSummand + additionalStr;
+        sum = EverySumToRightForm(sum);
+        result =" = "+ sum;
+        setOfDifferentSum.add(result);
+
+        if (!("" + secondSummand).equals("1")){
+            firstAddStr += "" +firstSummand+"+" ;
+            result += getNumberAsTwoSummands(firstAddStr +"", secondSummand, "", secondSummand, additionalStr);
+            firstAddStr = firstAddStr.substring(0,firstAddStr.length()-1-(""+firstSummand).length());
+        }
+        if (("" + secondSummand).equals("1")&&!(""+firstSummand).equals("1")&&count>1){
+            additionalStr += "+" + secondSummand; /*firstAddStr = firstAddStr.substring(0,firstAddStr.length()-1);*/
+            result += getNumberAsTwoSummands(firstAddStr, firstSummand, "", firstSummand, "" + additionalStr);
+            additionalStr = additionalStr.substring(0,additionalStr.length()-2);
+        }
+//        if (isFinishCalculation(firstSummand, secondSummand)){count--; return result;}
+        if (firstSummand==1){count--; return result;}
+//        if (firstSummand>=secondSummand){count--; return result;}
+        count--;
+        return getNumberAsTwoSummands(firstAddStr, number-1, result, firstNumber, additionalStr);
     }
 
-    private static String getNumberMoreThanTwoSummand(int number, String result, int firstNumber, String additionalStr){
-        if (number == 1) return result;
-        additionalStr += "+1";
-        result = getNumberAsTwoSummand(number-1, result,number-1, additionalStr);
-        return getNumberMoreThanTwoSummand(number -1, result, firstNumber, additionalStr);
+    private static String EverySumToRightForm(String str){
+        List<String> list = Arrays.asList(str.split("\\+"));
+        Collections.sort(list);
+        Collections.reverse(list);
+        String result = String.join("+", list);
+        return result;
     }
 
-    private static boolean isFinishCalculation(int number, int firstNumber){
-        return (firstNumber % 2 == 0 && number == firstNumber / 2) || (firstNumber % 2 != 0 && number == firstNumber / 2 + 1);
-    }
+
+
+//    private static void meth1(String result){
+//        String[] arr1 = result.split("=");
+//        List<String> list = Arrays.asList(meth2(0,arr1));
+//        TreeSet<String> set = new TreeSet<>(list);
+//        System.out.println(set.toString());
+//
+//    }
+//
+//    private static String[] meth2(int index, String[] arr){
+//        if (index == arr.length - 1) return arr;
+//        String[] arr2 = arr[index].split("");
+//
+//        Arrays.sort(arr2);
+//        arr[index] = Arrays.toString(arr2);
+//        index++;
+//        return meth2(index, arr);
+//    }
+
+
+
+
 
     public static void main(String[] args) {
-        System.out.println(asNumbersSum(7));
-
+       System.out.println(asNumbersSum(13));
     }
 }
