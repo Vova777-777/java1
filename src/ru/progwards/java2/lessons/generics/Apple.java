9186ee59d05b9a5bb2e8abc0884bedad37531922
Vova777 <vova.klok.00@mail.ru>
2021-05-14 11:12:57 +0300

    finish third task of third lesson

---
+++ src/ru/progwards/java2/lessons/generics/Apple.java
@@ -0,0 +1,8 @@
+package ru.progwards.java2.lessons.generics;
+
+public class Apple extends Fruit {
+    float weight = 1.0f;
+    public Apple() {
+        super.weight = weight;
+    }
+}
---
+++ src/ru/progwards/java2/lessons/generics/Orange.java
@@ -0,0 +1,8 @@
+package ru.progwards.java2.lessons.generics;
+
+public class Orange extends Fruit {
+    float weight = 1.5f;
+    public Orange() {
+        super.weight = weight;
+    }
+}
